defe8f2ce2397b18f5707c6b9c7ebdf738f76516
Brandon Croft <brandon.croft@gmail.com>
2017-09-13 14:01:14 -0600

    Node6 install

--- image/nodejs.sh
+++ image/nodejs.sh
@@ -3,8 +3,12 @@ set -e
 source /pd_build/buildconfig
 set -x
 
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+
 ## Install Node.js (also needed for Rails asset compilation)
-minimal_apt_get_install -y nodejs npm
+minimal_apt_get_install nodejs
+echo "+ Updating npm"
+run npm update npm -g
 if [[ ! -e /usr/bin/node ]]; then
 	ln -s /usr/bin/nodejs /usr/bin/node
 fi
